7caa92db788d94754c3215b6ac2abf83d255726f
FabPaul <paulfopa0@gmail.com>
2024-01-23 12:48:13 +0100

    hm

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,21 +7,16 @@ import pymongo
 
 def get_nginx_logs_stats(mongo_collection):
     """ function that provides nginx logs status """
- 
-    # get total logs
-    total_logs = mongo_collection.estimated_document_count({})
-    print(f"{total_logs} logs")
+    print(f"{mongo_collection.estimated_document_count()} logs")
 
-    # get HTTP methods stats
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-    for method in methods:
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = mongo_collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    # number of status log checked
-    status_count = mongo_collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_count} status check")
+    number_of_gets = mongo_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{number_of_gets} status check")
 
 
 if __name__ == "__main__":
