4c4b54c70df09746139eaa75c66ac75db8a4a58c
FabPaul <paulfopa0@gmail.com>
2024-01-23 17:17:43 +0100

    i prefer f strings

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,29 +2,30 @@
 """Python script that provides some stats about nginx logs stored in MongoDB"""
 
 from pymongo import MongoClient
+import pymongo
 
 if __name__ == "__main__":
-    # Initialize the database
-    client = MongoClient('mongodb://127.0.0.1:27017')
+
+    # generate the database
+    client = MongoClient('mongodb://localhost:27017')
     nginx_collection = client.logs.nginx
 
-    # Initialize list of methos
+    # generate list of methods
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     # Get total
     number_of_logs = nginx_collection.count_documents({})
 
     # Get count by methods
-
-    count_by_methods = [nginx_collection.count_documents(
-        {"method": method}) for method in methods]
+    count_by_methods = [nginx_collection.count_documents({"method": method}) for method in methods]
 
     count_status = nginx_collection.count_documents({"path": "/status"})
 
     # Prints
-    print("{} logs".format(number_of_logs))
+    print(f"{number_of_logs} logs")
     print("Methods:")
-    # number_of_documents = len(documents)
+
     for method, count in zip(methods, count_by_methods):
-        print("\tmethod {}: {}".format(method, count))
-    print("{} status check".format(count_status))
+        print(f"\tmethod {method}: {count}")
+
+    print(f"{count_status} status check")
