a98e645e3b1e4edde8b2323830f5635a2108be97
FabPaul <paulfopa0@gmail.com>
2024-01-24 15:28:25 +0100

    convert back to original format

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ Writng strings to redis """
 
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 import random
 import uuid
 
@@ -21,20 +21,24 @@ class Cache():
         self._redis.set(key, data)
 
         return key
-    
-    def get(self, key, fn):
-        """ Method that takes key and fn as callable """
+
+    def get(self, key: str, fn: Optional[Callable] = None
+            ) -> Union[str, bytes, int, float]:
+        """
+        Method that takes key and fn as callable
+        convert the data back to the desired format
+        """
         value = self._redis.get(key)
 
         if fn:
             value = fn(value)
             return value
-        
+
     def get_str(self, key) -> str:
         """ Method that gets a string format of a value """
         string = self.get(key, str)
         return string.decode("utf-8")
-    
+
     def get_int(self, key) -> int:
         """ Method that gets an int format of value """
         integer = self.get(key, int)
